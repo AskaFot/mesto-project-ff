ff93637ac42913dde08d85e7c39bb7adbb09a4be
AskaFot <sheptunova321@gmail.com>
2025-03-01 21:21:53 +0300

    working_version_of_the_site

--- src/scripts/cards.js
+++ src/scripts/cards.js
@@ -1,11 +1,11 @@
 // в файле cards.js описан массив карточек, отображаемых на странице;
 
-export const arkhyzImage = new URL('../images/arkhyz.jpg', import.meta.url);
-export const chelyabinskImage = new URL('../images/chelyabinsk-oblast.jpg', import.meta.url);
-export const ivanovoImage = new URL('../images/ivanovo.jpg', import.meta.url);
-export const kamchatkaImage = new URL('../images/kamchatka.jpg', import.meta.url);
-export const kholmogorskyImage = new URL('../images/kholmogorsky-rayon.jpg', import.meta.url);
-export const baikalImage = new URL('../images/baikal.jpg', import.meta.url);
+export const arkhyzImage = new URL('https://images.unsplash.com/photo-1691243915795-af2280b7c19b?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', import.meta.url);
+export const chelyabinskImage = new URL('https://images.unsplash.com/photo-1594751240727-ebe61ace8e93?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', import.meta.url);
+export const ivanovoImage = new URL('https://images.unsplash.com/photo-1641889176994-16f5926d753e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', import.meta.url);
+export const kamchatkaImage = new URL('https://images.unsplash.com/photo-1634745186518-db2e653372c9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', import.meta.url);
+export const kholmogorskyImage = new URL('https://images.unsplash.com/photo-1729002167138-d05965935bf0?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', import.meta.url);
+export const baikalImage = new URL('https://images.unsplash.com/photo-1617835594990-7cd5a9b5d153?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', import.meta.url);
 
 export const initialCards = [
   { name: 'Архыз', link: arkhyzImage },
